be9a291af9b983db2ddbef20b9f85c76447b090c
Paolo Monteverde <paolo.monteverde@cynny.com>
2017-05-12 01:28:39 +0200

    working project

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ NOTE: in order to build SDL2 for Android, you have to:
 	patch app/src/main/cpp/SDL/CMakeLists.txt app/src/main/cpp/patches/SDL2_CMakeLists.patch
 
 Note that this will modify your `SDL/CMakeLists.txt` file
+
+
+Credits
+====
+
+- The `main.cpp` file is from [stephen47](https://github.com/stephen47)'s [android-sdl2-gradle-template](https://github.com/stephen47/android-sdl2-gradle-template)
+- The [SDL](https://www.libsdl.org/) project of course; the `SDLActivity.java` file, in particular, comes (slightly adapted) from their Android template project
+
+
+Known issues and limitations
+====
+
+Currently, this project template support just a single ABI at a time; the problem I has not been able to solve is how to select the correct `libsdl` folder depending on the ABI name, so it's hardcoded for a single ABI.
--- app/src/main/java/io/github/morbo84/sdl2gradleexample/MainActivity.java
+++ app/src/main/java/io/github/morbo84/sdl2gradleexample/MainActivity.java
@@ -1,28 +1,8 @@
 package io.github.morbo84.sdl2gradleexample;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
+import org.libsdl.app.SDLActivity;
 
-public class MainActivity extends Activity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-}
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+public class MainActivity extends SDLActivity {}
